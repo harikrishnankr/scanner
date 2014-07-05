120f03db017e29aa0999e7b4b89cec87f867a19a
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2014-07-05 17:12:02 +0530

    Added recognized variable names

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -2,15 +2,15 @@ require_relative "scanner/version"
 
 module Scanner
   keyword=["int","float","char","double","long","short","signed","unsigned"];
-  user=gets.chomp;
-  user=user.gsub(',',' ');
-  user=user.split(" ");
-  user.each do |i|
-     if keyword.include?(i)
-          puts i;
-     elsif i.match(/^[_a-zA-Z][a-zA-Z_0-9]*$/)
+  words=gets.chomp;
+  words=words.gsub(',',' ');
+  words=words.split(" ");
+  words.each do |word|
+     if keyword.include?(word)
+          puts word;
+     elsif word.match(/^[_a-zA-Z][a-zA-Z_0-9]*$/)
           puts "identifier";
-     elsif i.match(/^[0-9][0-9]*$/)
+     elsif word.match(/^[0-9][0-9]*$/)
           puts "number";
      else
        puts "error";
