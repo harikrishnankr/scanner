dffa54fc6f3bb969127a2ae8b76f39c72c4bb25e
harikrishnan <you@example.com>
2014-07-05 16:32:41 +0530

    Identifier number keywords

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,6 +1,19 @@
 require_relative "scanner/version"
 
 module Scanner
-  puts "Haha this is our first compiler construction program";
-  puts "HolaHola";
+  keyword=["int","float","char","double","long","short","signed","unsigned"];
+  user=gets.chomp;
+  user=user.gsub(',',' ');
+  user=user.split(" ");
+  user.each do |i|
+     if keyword.include?(i)
+          puts i;
+     elsif i.match(/^[_a-zA-Z][a-zA-Z_0-9]*$/)
+          puts "identifier";
+     elsif i.match(/^[0-9][0-9]*$/)
+          puts "number";
+     else
+       puts "error";
+     end
+  end
 end
